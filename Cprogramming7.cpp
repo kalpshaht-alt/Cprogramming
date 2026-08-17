532da6d94602a3a5b3189f5df6616d6ff7140a34
kalpshaht-alt <kalp.shaht@gmail.com>
2026-08-18 03:49:04 +0530

    Add files via upload

---
+++ Cprogramming7.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    printf("Enter number of minutes 'a' : ");
+    scanf("%f",&a);
+    b = a / 60;
+    printf("'a' minutes in hours = %f",b);
+    return 0;
+
+}
