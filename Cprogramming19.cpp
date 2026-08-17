7cc91f7d181a81e97020c14f48cc1190965d96c1
kalpshaht-alt <kalp.shaht@gmail.com>
2026-08-18 04:01:48 +0530

    Add files via upload

---
+++ Cprogramming19.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   float a,b;
+   printf("Enter the radius of the circle : ");
+   scanf("%f",&a);
+   b = 3.14*a*a;
+   printf("Area of the circle of radius 'a' = %f ",b);
+   return 0;
+}
