6f4e7af856350384b23bd8e1c220dd7787871381
kalpshaht-alt <kalp.shaht@gmail.com>
2026-08-18 01:45:55 +0530

    Add files via upload

---
+++ Cprogramming.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h> //header file
+int main() //function
+{/*Program to add two numbers: */
+    int a,b,c;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+    c = a + b;
+    printf("%d + %d = %d",a,b,c);
+    return 0;
+
+}
