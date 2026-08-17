c23760309f130df8b85f6ab0ef37c80087450a07
kalpshaht-alt <kalp.shaht@gmail.com>
2026-08-18 03:49:59 +0530

    Add files via upload

---
+++ Cprogramming8.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    printf("Enter number of Dollars 'a' : ");
+    scanf("%f",&a);
+    b = 40 * a;
+    printf("Number of Rupees in 'a' Dollars = %f",b);
+    return 0;
+
+}
---
+++ Cprogramming9.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    printf("Enter number of Rupees 'a' : ");
+    scanf("%f",&a);
+    b = a / 40;
+    printf("'a' Rupees in Dollars = %f",b);
+    return 0;
+
+}
