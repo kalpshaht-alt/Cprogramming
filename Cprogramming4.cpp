38b75a09fd0cc6a1358265a56a076239a04b5076
kalpshaht-alt <kalp.shaht@gmail.com>
2026-08-18 03:47:47 +0530

    Add files via upload

---
+++ Cprogramming4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter number a : ");
+    scanf("%d",&a);
+    printf("Enter number b : ");
+    scanf("%d",&b);
+    c = a / b;
+    printf("%d / %d = %d",a,b,c);
+    return 0;
+}
